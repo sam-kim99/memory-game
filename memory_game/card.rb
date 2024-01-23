45c83614fbe7ed8d67e6ff8a5ac99d72c7cf1e04
sam-kim99 <samkim0451@DESKTOP-IMIKA2P>
2024-01-23 15:44:14 -0500

    update card class

--- memory_game/card.rb
+++ memory_game/card.rb
@@ -1,5 +1,28 @@
 class Card
-    def initialize()
+    def initialize
+        @face_up = false 
+        @face_down = false
+    end
+
+    def display
+        if @face_up
+            @grid[position]
+        end
+    end
+
+    def hide
+        if @face_up 
+            @face_down
+        end
+    end
+
+    def reveal
+        if @face_down
+            @face_up
+        end
+    end
+
+    def flip
 
     end
 end
